2b293d690d31e6276fa1b43a615dc8600b5da66b
Michał Macikowski <michal.m.1997@wp.pl>
2018-07-21 17:20:09 +0200

    no message

---
+++ SlavicGameJam2018/Assets/Scripts/LocalLeaderboard.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalLeaderboard : MonoBehaviour {
+
+    public class LeaderboardEntry {
+
+        public string playerName { get; set; }
+        public float score { get; set; }
+
+        public LeaderboardEntry(string name) {
+
+            playerName = name;
+            score = 0;
+
+        }
+    }
+
+    List<LeaderboardEntry> entries = new List<LeaderboardEntry>();
+
+    public void playerWon(string playerName) {
+        LeaderboardEntry referencedPlayer = entries.Find(entry => entry.playerName == playerName);
+        if (referencedPlayer != null) {
+            referencedPlayer.score += 2;
+        }
+        else {
+            LeaderboardEntry newPlayer = new LeaderboardEntry(playerName);
+            newPlayer.score += 2;
+            entries.Add(newPlayer);
+        }
+    }
+
+    public void playerLost(string playerName) {
+        LeaderboardEntry referencedPlayer = entries.Find(entry => entry.playerName == playerName);
+        if (referencedPlayer != null) {
+            referencedPlayer.score -= 1;
+        }
+        else {
+            LeaderboardEntry newPlayer = new LeaderboardEntry(playerName);
+            newPlayer.score -= 1;
+            entries.Add(newPlayer);
+        }
+    }
+
+    public void AddNewPlayer(string newPlayerName) {
+        LeaderboardEntry referencedPlayer = entries.Find(entry => entry.playerName == newPlayerName);
+        if (referencedPlayer != null) {
+            //TODO Username already exists
+        }
+        else {
+            LeaderboardEntry newPlayer = new LeaderboardEntry(newPlayerName);
+            entries.Add(newPlayer);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
